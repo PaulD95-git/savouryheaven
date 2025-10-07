9dea9bf66385f1303fbfb3b2079371d3655babb6
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-10-07 13:56:11 +0100

    DEBUG set to false

--- savouryheaven/settings.py
+++ savouryheaven/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Hosts allowed to serve the project
 ALLOWED_HOSTS = ['savouryheaven.herokuapp.com', 'localhost', '127.0.0.1']
